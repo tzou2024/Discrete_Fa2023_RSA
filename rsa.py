ef0e19ae9a3d169b75e1b4e16e50de788f19c18c
tzou2024 <tzou@olin.edu>
2023-11-30 00:03:49 -0500

    Add basic encryption and decryption

--- rsa.py
+++ rsa.py
@@ -16,10 +16,23 @@ def mod_inverse(e, phi):
             return d
     return ValueError
 
+def euler_totient(p, q):
+    #I think that this is a shortcut
+    phi_n = (p - 1) * (q - 1)
+    return phi_n
+
+def lcm(a, b):
+    return abs(a*b) // bltin_gcd(a, b)
+
+def carmichael_function(p, q):
+    rho_n = lcm((p-1),(q-1))
+    return rho_n
+
 def rsa(min, max):
     #first choose p and q as two prime numbers that are different from each other
 	p = generate_prime(min, max)
 	q = generate_prime(min, max)
+
 	while p == q:
 		q = generate_prime(min, max)
 	
@@ -27,27 +40,39 @@ def rsa(min, max):
 	n = p * q
 	
 	#Eulers totients
-	phi_n = (p - 1) * (q - 1)
+	totient_n = euler_totient(p, q) #either use gcd or lcm 
  
 	#n can be made public, p q and phi stay secret
  
 	# pick an public key e e such that 2 < e < phi(n) and gcd(e, phi(n) = 1
-	
-	e = random.randint(2, phi_n)
-	while not coprime2(e, phi_n):
-		e = random.randint(2, phi_n)
+	e = random.randint(2, totient_n)
+	while not coprime2(e, totient_n):
+		e = random.randint(2, totient_n)
 
 	# calculate a private key d such that e * d == 1 mod(phi(n)) || e * d mod phi(n) = 1
-	d = mod_inverse(e, phi_n)
+	d = mod_inverse(e, totient_n)
 	
 	#public information is e and n 
 	#prive is d
 	pub = {"e":e, "n":n}
-	priv = {"d":d}
+	priv = {"d":d, "n":n}
 	return pub, priv
 
 # def encrypt(m, pub):
+
+def encrypt(m, pub):
+    return (m ** pub["e"]) % pub["n"]
+
+def decrypt(c, priv):
+    return c ** priv["d"] % pub["n"]
     
  
-pub, priv = rsa(0, 20)
-print(pub, priv)
+pub, priv = rsa(0, 200)
+
+m = 4
+
+c = encrypt(m, pub)
+
+m2 = decrypt(c, priv)
+
+print(m == m2)
