4909ecbbb69d3aa2042923cdfaab345df5732edf
tzou2024 <tzou@olin.edu>
2023-12-04 11:46:51 -0500

    Speed up random prime generation lru caching

--- rsa.py
+++ rsa.py
@@ -2,10 +2,13 @@
 import random 
 from primePy import primes
 from math import gcd as bltin_gcd
+from functools import lru_cache
 
+# Memoized using LRU Cache
+@lru_cache(maxsize=None)
 def generate_prime(min, max):
     available = primes.between(min, max)
-    return random.choice(available)
+    return random.choice(available), random.choice(available)
 
 def coprime2(a, b):
     return bltin_gcd(a, b) == 1
@@ -30,8 +33,9 @@ def carmichael_function(p, q):
 
 def rsa(min, max):
     #first choose p and q as two prime numbers that are different from each other
-	p = generate_prime(min, max)
-	q = generate_prime(min, max)
+	(p,q) = generate_prime(min, max)
+
+	print("generate p and q")
 
 	while p == q:
 		q = generate_prime(min, max)
@@ -40,16 +44,19 @@ def rsa(min, max):
 	n = p * q
 	
 	#Eulers totients
+	print("totient")
 	totient_n = euler_totient(p, q) #either use gcd or lcm 
  
 	#n can be made public, p q and phi stay secret
  
 	# pick an public key e e such that 2 < e < phi(n) and gcd(e, phi(n) = 1
+	print("e")
 	e = random.randint(2, totient_n)
 	while not coprime2(e, totient_n):
 		e = random.randint(2, totient_n)
 
 	# calculate a private key d such that e * d == 1 mod(phi(n)) || e * d mod phi(n) = 1
+	print("d")
 	d = mod_inverse(e, totient_n)
 	
 	#public information is e and n 
@@ -64,15 +71,17 @@ def encrypt(m, pub):
     return (m ** pub["e"]) % pub["n"]
 
 def decrypt(c, priv):
-    return c ** priv["d"] % pub["n"]
+    return (c ** priv["d"]) % pub["n"]
     
  
-pub, priv = rsa(0, 200)
+print("generating")
+pub, priv = rsa(2**8, 3**8)
+print(pub, priv)
 
 m = 4
-
+print("encrypt")
 c = encrypt(m, pub)
-
+print("decrypt")
 m2 = decrypt(c, priv)
 
 print(m == m2)
