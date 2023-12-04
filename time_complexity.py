6f6ef3e212dd66d08077a82b835b9adf45841da0
An Grocki <angrocki02@gmail.com>
2023-12-04 12:42:13 -0500

    Save data to csv

--- time_complexity.py
+++ time_complexity.py
@@ -1,7 +1,7 @@
 '''
 Testing time complexity of RSA implementations
 '''
-
+import csv
 import time 
 import rsa
 
@@ -14,14 +14,19 @@ def bit_length_complexity(min, max, step):
     """
     min_value = 2**min 
     max_value = 2**max 
-
-    for size in range (min, max-step, step):
+    data = dict()
+    for size in range (min, max, step):
         min_value = 2**size 
         max_value = 2**(size+step)
         avg_time = get_average_RSA(min_value, max_value, REPETITION)
+        data[f"{size}-{size+step}"] = avg_time
         print(f"Time from {size} bits to {size+step} bits: {avg_time}")
-    
 
+    with open(f"{min}-{max}-step{step}.csv", 'w', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=data.keys())
+        writer.writeheader()
+        writer.writerow(data)
+        
 # Finding average time
 def get_average_RSA(min, max, rep):
     """
@@ -44,4 +49,4 @@ def get_average_RSA(min, max, rep):
     return average
 
 
-bit_length_complexity(4, 24, 4)
+bit_length_complexity(3, 8, 1)
